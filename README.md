7e4c8fbf5c178cd12b6a9e482745c6a319625688
Jerry <calljerryli@outlook.com>
2019-03-15 08:43:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 夸夸生成器
+
+更多词语请打开 `data/*.txt` 自行补充
+
+# 套路分析
+
+![](images/analyze.jpg)
+
+# 如何运行
+
+Windows 用户：下载压缩包解压后双击运行 `main.exe`
+
+*nux 用户：克隆源码，`go run main.go`
+
+# 使用示例
+
+![](images/demo.png)
+
+# Just For Fun
+
+请各位不要当真
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	var opt int
+	for {
+		fmt.Print("\n1)夸智慧 2)夸仪态 3)夸口才 4)夸品质\n>>> ")
+		_, err := fmt.Scan(&opt)
+		if err != nil {
+			fmt.Println("I am not playing with you.")
+			return
+		}
+
+		shock := readWords("data/惊叹.txt")
+		source := readWords("data/来源.txt")
+		goodGoodStudyDayDayUp := readWords("data/事业祝福语.txt")
+		var praise [][]string
+		switch opt {
+		case 1:
+			praise = readWords("data/智慧.txt")
+			break
+		case 2:
+			praise = readWords("data/仪态.txt")
+			break
+		case 3:
+			praise = readWords("data/口才.txt")
+			break
+		case 4:
+			praise = readWords("data/品质.txt")
+			break
+		}
+
+		fmt.Println(
+			shock[rand.Intn(len(shock))][0] +
+				"，从" +
+				source[rand.Intn(len(source))][0] +
+				"可以看得出你是个" +
+				praise[rand.Intn(len(praise))][0] +
+				"、" +
+				praise[rand.Intn(len(praise))][0] +
+				"、" +
+				praise[rand.Intn(len(praise))][0] +
+				"的人，日后的道路一定会" +
+				goodGoodStudyDayDayUp[rand.Intn(len(goodGoodStudyDayDayUp))][0] +
+				"、" +
+				goodGoodStudyDayDayUp[rand.Intn(len(goodGoodStudyDayDayUp))][0] +
+				"！")
+	}
+}
+
+func readWords(filename string) (records [][]string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	reader := csv.NewReader(f)
+	records, err = reader.ReadAll()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	f.Close()
+	return
+}
